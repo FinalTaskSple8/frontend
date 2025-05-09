8130855a0d58976ee8dc56068eb351307b0d6559
claraa26 <clarasistaa26@gmail.com>
2025-05-09 21:52:15 +0700

    coba deploy lagi2

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import checker from "vite-plugin-checker";
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
+    root: "src",
     base: "/",
     plugins: [
       react(),
       svgr(),
     ],
-    publicDir: "public",
+    publicDir: process.cwd() + "/public",
     resolve: {
       alias: {
-        "@": "/src",
+        "@": process.cwd() + "/src",
       },
     },
     server: {
@@ -28,8 +34,9 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    envDir: process.cwd(),
     build: {
-      outDir: "dist", // kembali ke default
+      outDir: process.cwd() + "/build",
       emptyOutDir: true,
       commonjsOptions: {
         transformMixedEsModules: true,
